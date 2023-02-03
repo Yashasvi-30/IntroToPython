b8eeed6f173989af00330d0b3a4d97aa4544c247
Yashasvi Gupta <Yashasvi.101768@stu.upes.ac.in>
2023-02-03 20:50:26 +0530

    Add files via upload

---
+++ StarPattern2.py
@@ -0,0 +1,11 @@
+n=int(input("Enter the number of rows you want." ))
+
+for i in range(1, n+1):
+    
+    for j in range(n, 0, -1):
+        if j > i:
+            print(" ", end=' ')
+        else:
+            print("*", end=' ')
+            
+    print("")            
