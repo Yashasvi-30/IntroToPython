a558e7da373080045c228ea1337e2df5b5937cf0
Yashasvi Gupta <Yashasvi.101768@stu.upes.ac.in>
2023-02-14 16:26:42 +0530

    Delete NumberPatterns directory

--- NumberPatterns/NumberPattern5.py
+++
@@ -1,8 +0,0 @@
-#To print the number pattern 4
-n= int (input ("Enter the Number of rows you want. "))
-m = int(input ("Enter the digit to print the pattern. "))
-
-for i in range (1,n+1):
-    for j in range (n,i-1, -1):
-            print (m , end=" ")
-    print ()
