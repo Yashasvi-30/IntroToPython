372bf924deba5517d5bdc85197817180ec2457dd
Yashasvi Gupta <Yashasvi.101768@stu.upes.ac.in>
2023-02-21 17:28:08 +0530

    Add files via upload

---
+++ If-Else Conditionals/PosNeg.py
@@ -0,0 +1,8 @@
+#To check if a number is positive or negative
+num = float(input("Enter a number: "))
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number")
